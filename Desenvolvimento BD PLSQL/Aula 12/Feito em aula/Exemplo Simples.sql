603fee0f24bfe28d020b26c43b0145c5630e10b1
luiscarlosjunior <luis.carlosjr@hotmail.com.br>
2020-06-21 22:34:22 -0300

    Updated code

--- Desenvolvimento BD PLSQL/Aula 12/Feito em aula/Exemplo Simples.sql
+++ Desenvolvimento BD PLSQL/Aula 12/Feito em aula/Exemplo Simples.sql
@@ -9,12 +9,17 @@ END pacote_exibir_texto;
 CREATE OR REPLACE PACKAGE BODY pacote_exibir_texto IS
   PROCEDURE proc_hello IS
   BEGIN
-    DBMS_OUTPUT.PUT_LINE('Hello world');
+    DBMS_OUTPUT.PUT_LINE('Hello world, Proc');
   END;
   
   FUNCTION func_hello RETURN VARCHAR2 IS
   BEGIN
-    RETURN('Hello World');
+    RETURN('Hello World, Func');
   END;
 END;
 /
+
+-- Executar o procedimento dentro do pacote
+exec pacote_exibir_texto.proc_hello;
+
+SELECT pacote_exibir_texto.func_hello FROM DUAL;
